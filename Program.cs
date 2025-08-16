6f30de9e11a62dc7dce07c4cc9e468d290e34abf
Palash Das <palash2k5@gmail.com>
2025-08-16 20:40:16 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub Actions with .NET Framework 4.8!");
+        }
+    }
+}
